505c87ef8768d6b510abc085066ad8f265ec4cb6
david de la puente enriquez <Twinkymen@hotmail.com>
2022-03-29 09:40:07 +0200

    Initial commit

---
+++ src/test/java/com/david/rockets/RocketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.rockets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
